824f15fb692c6abd952e3010db81bb7d74a9ca56
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-12 12:19:35 +0530

    Update PR4(1).C

---
+++ PR4(1).C
@@ -0,0 +1,25 @@
+// q1
+
+
+#include <stdio.h>
+void main() 
+
+{
+   int i, j, rows;
+   
+printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   
+for (i = 41; i <= rows; ++i) 
+{
+      for (j = 41; j <= i; ++j) 
+
+{
+      printf("%d ", j);
+      
+}
+      printf("\n");
+  
+}
+   getch();
+}
